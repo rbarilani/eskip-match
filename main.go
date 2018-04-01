471166171f615a752af8a1324b08ec30159539bd
Ruben Barilani <ruben.barilani@zalando.de>
2018-04-01 18:29:45 +0200

    refactor: reduce number of public symbols

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/rbarilani/eskip-match/command"
+	"github.com/rbarilani/eskip-match/cli"
 )
 
 func main() {
-	app := command.NewApp()
+	app := cli.NewApp()
 	app.Version = "0.3.1"
 	err := app.Run(os.Args)
 	if err != nil {
